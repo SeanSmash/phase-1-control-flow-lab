f50a5d438aefb93bf2c6050529bef30f94986944
Sean Sarokas <sps4462@gmail.com>
2022-04-07 20:20:17 -0400

    commit tree 4e378eb8dc249748e67bf4185aaca4d775774ca0

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let chargeResponse;
+  if (someValue <= 400) {
+    chargeResponse = `This one is on me!`;
+  }
+  else if (someValue <= 2000) {
+    chargeResponse = `That will be twenty bucks.`;
+  }
+  else if (someValue <= 2500) {
+    chargeResponse = `I will gladly take your thirty bucks.`;
+  }
+  else if (someValue > 2500) {
+    chargeResponse = `No can do.`;
+  }
+  return chargeResponse;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  let cityResponse;
+  someCity === "NYC" ? (cityResponse = "Ok, sounds good.") : (cityResponse = "No go.");
+  return cityResponse;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTip){
+  let tipResponse;
+  switch (someTip) {
+    case "generous" :
+      tipResponse = "Thank you so much.";
+      break;
+    case "not as generous" :
+      tipResponse = "Thank you.";
+      break;
+    default :
+      tipResponse = "Bye.";
+      break;
+  }
+  return tipResponse;
 }
